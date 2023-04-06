8a2c5f7e84c9cc0cc7f584297ea644022a1ed5b4
Maxim <59160733+Deviance-Dev@users.noreply.github.com>
2023-04-06 10:44:12 +0300

    Update extractors.py
    Add commenter_profile avatar

--- facebook_scraper/extractors.py
+++ facebook_scraper/extractors.py
@@ -1053,6 +1053,7 @@ class PostExtractor:
             profile_picture = comment.find(".profpic.img", first=True)
             name = profile_picture.attrs.get("alt") or profile_picture.attrs.get("aria-label")
             name = name.split(",")[0]
+            commenter_avatar = profile_picture.attrs.get("href")
             commenter_id = re.search(r'feed_story_ring(\d+)', comment.html)
             if commenter_id:
                 commenter_id = commenter_id.group(1)
@@ -1134,6 +1135,7 @@ class PostExtractor:
             "commenter_url": url,
             "commenter_name": name,
             "commenter_meta": commenter_meta,
+            "commenter_avatar": commenter_avatar,
             "comment_text": text,
             "comment_time": date,
             "comment_image": image_url,
